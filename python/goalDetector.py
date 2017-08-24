28e2d18a57eed98b927f97e8f8eb9e632ff02e48
Ali Taheri <ali.taheri@gmail.com>
2017-08-24 01:23:35 +0000

    add initial python script

---
+++ python/goalDetector.py
@@ -0,0 +1,132 @@
+import time
+import threading
+from gpiozero import LightSensor, OutputDevice
+
+from neopixel import *
+import atexit
+
+
+
+# LED strip configuration:
+LED_COUNT      = 150      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
+
+
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+	"""Wipe color across display a pixel at a time."""
+	for i in range(strip.numPixels()):
+		strip.setPixelColor(i, color)
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+
+def theaterChase(strip, color, wait_ms=50, iterations=10):
+	"""Movie theater light style chaser animation."""
+	for j in range(iterations):
+		for q in range(3):
+			for i in range(0, strip.numPixels(), 3):
+				strip.setPixelColor(i+q, color)
+			strip.show()
+			time.sleep(wait_ms/1000.0)
+			for i in range(0, strip.numPixels(), 3):
+				strip.setPixelColor(i+q, 0)
+
+def wheel(pos):
+	"""Generate rainbow colors across 0-255 positions."""
+	if pos < 85:
+		return Color(pos * 3, 255 - pos * 3, 0)
+	elif pos < 170:
+		pos -= 85
+		return Color(255 - pos * 3, 0, pos * 3)
+	else:
+		pos -= 170
+		return Color(0, pos * 3, 255 - pos * 3)
+
+def rainbow(strip, wait_ms=20, iterations=1):
+	"""Draw rainbow that fades across all pixels at once."""
+	for j in range(256*iterations):
+		for i in range(strip.numPixels()):
+			strip.setPixelColor(i, wheel((i+j) & 255))
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+
+def rainbowCycle(strip, wait_ms=20, iterations=5):
+	"""Draw rainbow that uniformly distributes itself across all pixels."""
+	for j in range(256*iterations):
+		for i in range(strip.numPixels()):
+			strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+
+def theaterChaseRainbow(strip, wait_ms=50):
+	"""Rainbow movie theater light style chaser animation."""
+	for j in range(256):
+		for q in range(3):
+			for i in range(0, strip.numPixels(), 3):
+				strip.setPixelColor(i+q, wheel((i+j) % 255))
+			strip.show()
+			time.sleep(wait_ms/1000.0)
+			for i in range(0, strip.numPixels(), 3):
+				strip.setPixelColor(i+q, 0)
+
+def showGoal(side):
+    if (side == 1):
+        theaterChase(strip,Color(127,0,0))
+    else:
+        theaterChase(strip, Color(0,0,127))
+
+def blueGoal():
+    #setup light sensor
+    ldr = LightSensor(27,5,0.022)
+    
+    while True:
+        ldr.wait_for_dark()
+        print("Blue Goal")
+        theaterChase(strip, Color(0,0,127))
+
+def redGoal():
+    #setup light sensor
+    ldr = LightSensor(4,5,0.022)
+    
+    while True:
+        ldr.wait_for_dark()
+        print("Red Goal")
+        theaterChase(strip,Color(127,0,0))
+
+def exit_handler():
+    diode.off()
+
+def main():
+    red = threading.Thread(target=redGoal)
+    blue = threading.Thread(target=blueGoal)
+
+    red.start()
+    blue.start()
+
+    while True:
+        pass
+        #ldr.wait_for_dark()
+        #showGoal(2)
+
+#start process
+if __name__ == '__main__':
+    #setup laser diode
+    diode = OutputDevice(17)
+    diode.on()
+
+
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+	
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+    
+    main()
