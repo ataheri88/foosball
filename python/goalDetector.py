9c05f4cfaf885a715a83a73106f7bb3ce27527f3
Ali Taheri <ali.taheri@gmail.com>
2017-08-25 18:14:41 +0000

    add quit capability

--- python/goalDetector.py
+++ python/goalDetector.py
@@ -1,10 +1,10 @@
 import time
 import threading
+import signal
+
 from gpiozero import LightSensor, OutputDevice
 
 from neopixel import *
-import atexit
-
 
 
 # LED strip configuration:
@@ -19,6 +19,7 @@ LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 
 
+_quit_event = threading.Event()
 
 # Define functions which animate LEDs in various ways.
 def colorWipe(strip, color, wait_ms=50):
@@ -77,56 +78,85 @@ def theaterChaseRainbow(strip, wait_ms=50):
 			for i in range(0, strip.numPixels(), 3):
 				strip.setPixelColor(i+q, 0)
 
-def showGoal(side):
-    if (side == 1):
-        theaterChase(strip,Color(127,0,0))
-    else:
-        theaterChase(strip, Color(0,0,127))
-
-def blueGoal():
+def blueGoal(stop_event):
     #setup light sensor
     ldr = LightSensor(27,5,0.022)
-    
-    while True:
-        ldr.wait_for_dark()
-        print("Blue Goal")
-        theaterChase(strip, Color(0,0,127))
 
-def redGoal():
+    #setup laser diode
+    diode = OutputDevice(6)
+    diode.on()
+
+    while not stop_event.is_set():
+        if ldr.wait_for_dark(1):
+            print("Blue Goal")
+            theaterChase(strip, Color(0,0,127))
+
+    diode.off()
+    ldr.close()
+
+    print("blue  Done")
+
+def redGoal(stop_event):
     #setup light sensor
     ldr = LightSensor(4,5,0.022)
-    
-    while True:
-        ldr.wait_for_dark()
-        print("Red Goal")
-        theaterChase(strip,Color(127,0,0))
 
-def exit_handler():
-    diode.off()
-
-def main():
-    red = threading.Thread(target=redGoal)
-    blue = threading.Thread(target=blueGoal)
-
-    red.start()
-    blue.start()
-
-    while True:
-        pass
-        #ldr.wait_for_dark()
-        #showGoal(2)
-
-#start process
-if __name__ == '__main__':
     #setup laser diode
     diode = OutputDevice(17)
     diode.on()
 
+    while not stop_event.is_set():
+        if ldr.wait_for_dark(1):
+            print("Red Goal")
+            theaterChase(strip,Color(127,0,0))
+
+    diode.off()
+    ldr.close()
+
+    print("red  Done")
+
+def exit_handler(signal, frame):
+   print "Quiting!"
+   _quit_event.set()
+
+def handle_input_command(stop_event):
+    while not stop_event.is_set():
+        command = raw_input('Command:')
+        print command.lower()
+
+        if command.lower() == "quit":
+            _quit_event.set()
+
+    print "input thread quiting"
+
+def main():
+
+    red = threading.Thread(target=redGoal, args=(_quit_event,))
+    blue = threading.Thread(target=blueGoal, args=(_quit_event,))
+    input_thread = threading.Thread(target=handle_input_command, args=(_quit_event,))
+    input_thread.daemon = True
+
+    red.start()
+    blue.start()
+    input_thread.start()
+
+    while not _quit_event.is_set():
+        pass
+
+    # wait for other threads to quit and clean up before main can exit
+    red.join()
+    blue.join()
+    input_thread.join(2)
+    print("Main Done")
+
+#start process
+if __name__ == '__main__':
+
+    signal.signal(signal.SIGINT, exit_handler)
 
     # Create NeoPixel object with appropriate configuration.
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-	
+
     # Intialize the library (must be called once before other functions).
     strip.begin()
-    
+
     main()
