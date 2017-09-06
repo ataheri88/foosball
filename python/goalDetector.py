e742e6127cccdfaa025e4af93ea5bef79fbd03c1
Ali Taheri <ali.taheri@gmail.com>
2017-09-06 12:37:52 -0700

    change tabs to spaces

--- python/goalDetector.py
+++ python/goalDetector.py
@@ -31,82 +31,82 @@ _lastGoalTime = 0
 
 # Define functions which animate LEDs in various ways.
 def colorWipe(strip, color, wait_ms=50):
-	"""Wipe color across display a pixel at a time."""
-	for i in range(strip.numPixels()):
-		strip.setPixelColor(i, color)
-		strip.show()
-		time.sleep(wait_ms/1000.0)
+  """Wipe color across display a pixel at a time."""
+  for i in range(strip.numPixels()):
+    strip.setPixelColor(i, color)
+    strip.show()
+    time.sleep(wait_ms/1000.0)
 
 def theaterChase(strip, color, wait_ms=50, iterations=10):
-	"""Movie theater light style chaser animation."""
-	for j in range(iterations):
-		for q in range(3):
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, color)
-			strip.show()
-			time.sleep(wait_ms/1000.0)
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, 0)
+  """Movie theater light style chaser animation."""
+  for j in range(iterations):
+    for q in range(3):
+      for i in range(0, strip.numPixels(), 3):
+        strip.setPixelColor(i+q, color)
+      strip.show()
+      time.sleep(wait_ms/1000.0)
+      for i in range(0, strip.numPixels(), 3):
+        strip.setPixelColor(i+q, 0)
 
 def wheel(pos):
-	"""Generate rainbow colors across 0-255 positions."""
-	if pos < 85:
-		return Color(pos * 3, 255 - pos * 3, 0)
-	elif pos < 170:
-		pos -= 85
-		return Color(255 - pos * 3, 0, pos * 3)
-	else:
-		pos -= 170
-		return Color(0, pos * 3, 255 - pos * 3)
+  """Generate rainbow colors across 0-255 positions."""
+  if pos < 85:
+    return Color(pos * 3, 255 - pos * 3, 0)
+  elif pos < 170:
+    pos -= 85
+    return Color(255 - pos * 3, 0, pos * 3)
+  else:
+    pos -= 170
+    return Color(0, pos * 3, 255 - pos * 3)
 
 def rainbow(strip, wait_ms=20, iterations=1):
-	"""Draw rainbow that fades across all pixels at once."""
-	for j in range(256*iterations):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel((i+j) & 255))
-		strip.show()
-		time.sleep(wait_ms/1000.0)
+  """Draw rainbow that fades across all pixels at once."""
+  for j in range(256*iterations):
+    for i in range(strip.numPixels()):
+      strip.setPixelColor(i, wheel((i+j) & 255))
+    strip.show()
+    time.sleep(wait_ms/1000.0)
 
 def rainbowCycle(strip, wait_ms=20, iterations=5):
-	"""Draw rainbow that uniformly distributes itself across all pixels."""
-	for j in range(256*iterations):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
-		strip.show()
-		time.sleep(wait_ms/1000.0)
+  """Draw rainbow that uniformly distributes itself across all pixels."""
+  for j in range(256*iterations):
+    for i in range(strip.numPixels()):
+      strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+    strip.show()
+    time.sleep(wait_ms/1000.0)
 
 def theaterChaseRainbow(strip, wait_ms=50):
-	"""Rainbow movie theater light style chaser animation."""
-	for j in range(256):
-		for q in range(3):
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, wheel((i+j) % 255))
-			strip.show()
-			time.sleep(wait_ms/1000.0)
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, 0)
+  """Rainbow movie theater light style chaser animation."""
+  for j in range(256):
+    for q in range(3):
+      for i in range(0, strip.numPixels(), 3):
+        strip.setPixelColor(i+q, wheel((i+j) % 255))
+      strip.show()
+      time.sleep(wait_ms/1000.0)
+      for i in range(0, strip.numPixels(), 3):
+        strip.setPixelColor(i+q, 0)
 
 def turnLedsOff(strip):
-	for i in range(strip.numPixels()):
-		strip.setPixelColor(i,Color(0,0,0))
-	
-	strip.show()
+  for i in range(strip.numPixels()):
+    strip.setPixelColor(i,Color(0,0,0))
+
+  strip.show()
 
 def goalDetected(channel):
-	if (time.time() - _lastGoalTime < MIN_TIME_BETWEEN_GOALS_SEC):
-		return
+  if (time.time() - _lastGoalTime < MIN_TIME_BETWEEN_GOALS_SEC):
+    return
 
-	# Timestamp time of this goal
-	_lastGoalTime = time.time()
+  # Timestamp time of this goal
+  _lastGoalTime = time.time()
 
-	if (channel == BLUE_LDR_PIN):
-		print("Blue")
-  	theaterChase(strip, Color(0,0,127))
-	else:
-		print("Red")
-  	theaterChase(strip,Color(127,0,0))
-    
-	turnLedsOff(strip)
+  if (channel == BLUE_LDR_PIN):
+    print("Blue")
+    theaterChase(strip, Color(0,0,127))
+  else:
+    print("Red")
+    theaterChase(strip,Color(127,0,0))
+
+  turnLedsOff(strip)
 
 def exit_handler(signal, frame):
    print "Quiting!"
@@ -142,7 +142,7 @@ def main():
 
     GPIO.add_event_detect(BLUE_LDR_PIN, GPIO.RISING, callback=goalDetected, bouncetime=3000)
     GPIO.add_event_detect(RED_LDR_PIN,  GPIO.RISING, callback=goalDetected, bouncetime=3000)
-    
+
     _quit_event.wait()
 
     GPIO.remove_event_detect(BLUE_LDR_PIN)
@@ -157,10 +157,10 @@ def main():
     GPIO.cleanup(RED_LDR_PIN)
     GPIO.cleanup(RED_DIODE_PIN)
 
-    
+
     # wait for other threads to quit and clean up before main can exit
     input_thread.join(2)
-    
+
     print("Main Done")
 
 #start process
